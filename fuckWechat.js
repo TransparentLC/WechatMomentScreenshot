2539f1e73be125e40b7ba93bfc44dc37164d9d8e
✨小透明・宸✨ <47057319+TransparentLC@users.noreply.github.com>
2023-10-13 23:05:52 +0800

    Update fuckWechat.js

--- fuckWechat.js
+++ fuckWechat.js
@@ -7,11 +7,9 @@
     div.innerHTML = ''
       + '<div>'
       +     '<h1>(　^ω^)</h1>'
-      +     '<p>为什么你还在使用这个<a href="https://www.zhihu.com/question/271184234" target="_blank" style="color:#175199">功能残缺</a>的辣鸡社交软件？</p>'
-      +     '<p>一个必须使用手机扫🐴才能登录电脑版客户端的软件有什么使用的必要吗？</p>'
-      +     '<p>隔壁的 QQ 不好用吗？</p>'
+      +     '<p>本工具不建议在微信中使用。</p>'
       +     '<p><button style="width:100%;background-color:#175199;color:#fff;border:none;border-radius:4px;height:2em" onclick="document.body.removeChild(document.getElementById(\'fuckWechat\'));document.querySelector(\'.mdui-appbar\').style.filter=\'\';document.querySelector(\'.mdui-container\').style.filter=\'\';">继续</button></p>'
-      +     '<p style="text-align:center;color:#bbb;font-size:14px">你没看错，这个遮罩层就是来 yygq 的<br>在微信以外的环境打开此页面，不会出现此遮罩层</p>'
+      +     '<p style="text-align:center;color:#bbb;font-size:14px">在微信以外的环境打开此页面，不会出现此遮罩层</p>'
       + '</div>';
 
     document.body.append(div);
